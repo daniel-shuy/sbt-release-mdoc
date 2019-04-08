078a2ace9fa5b9b1b389a0ca6cbdaaa324768a93
Daniel Shuy <daniel_shuy@hotmail.com>
2019-04-08 23:35:48 +0800

    Fix ReleaseMdocStateTransformations#commitMdoc to commit output files created/changed by mdoc

--- src/main/scala/com/github/daniel/shuy/sbt/release/mdoc/ReleaseMdocStateTransformations.scala
+++ src/main/scala/com/github/daniel/shuy/sbt/release/mdoc/ReleaseMdocStateTransformations.scala
@@ -1,12 +1,17 @@
 package com.github.daniel.shuy.sbt.release.mdoc
 
 import mdoc.MdocPlugin
-import sbt.{IO, State}
+import mdoc.internal.cli.{MainOps, MdocProperties, Settings}
+import sbt.Keys.{classDirectory, dependencyClasspath, scalacOptions}
+import sbt.{Compile, File, Project, State}
 import sbtrelease.ReleasePlugin.autoImport.{ReleaseStep, releaseVcs}
 import sbtrelease.Utilities._
 import sbtrelease.{ReleasePlugin, ReleaseStateTransformations, Vcs}
 
+import scala.collection.mutable.ListBuffer
 import scala.language.postfixOps
+import scala.meta.internal.io.PathIO
+import scala.meta.io.AbsolutePath
 import scala.sys.process.ProcessLogger
 
 object ReleaseMdocStateTransformations {
@@ -14,33 +19,69 @@ object ReleaseMdocStateTransformations {
   /**
     * [[ReleaseStep]] to commit mdoc changes.
     * @see [[ReleaseStateTransformations.commitNextVersion]]
+    * @see [[MainOps.generateCompleteSite()]]
+    * @see [[MdocPlugin.projectSettings]]
+    * @see [[MdocProperties.fromProps()]]
+    * @see [[Settings.default()]]
     */
-  lazy val commitMdoc = ReleaseStep(st => {
-    val log = toProcessLogger(st)
-    val directory =
-      st.extract.get(MdocPlugin.autoImport.mdocIn).getCanonicalFile
-    val base = vcs(st).baseDir.getCanonicalFile
-    val sign = st.extract.get(ReleasePlugin.autoImport.releaseVcsSign)
-    val signOff = st.extract.get(ReleasePlugin.autoImport.releaseVcsSignOff)
-    val relativePath = IO
-      .relativize(base, directory)
-      .getOrElse(
-        "mdoc input directory [%s] is outside of this VCS repository with base directory [%s]!" format (directory, base))
+  lazy val commitMdoc = ReleaseStep(action = st => {
+    val classpath = ListBuffer.empty[File]
+    val compileDependencyClasspath =
+      Project.runTask(dependencyClasspath.in(Compile), st)
+    compileDependencyClasspath
+      .map(_._2.toEither)
+      .map(_.map(_.iterator.map(_.data)))
+      .foreach(_.foreach(classpath ++= _))
+    classpath += st.extract.get(classDirectory.in(Compile))
 
-    vcs(st).add(relativePath) !! log
-    val status = (vcs(st).status !!) trim
+    val propsDefaultScalacOptions = MdocProperties(
+      classpath = classpath.mkString(java.io.File.pathSeparator),
+      site = st.extract.get(MdocPlugin.autoImport.mdocVariables),
+      in = Option(AbsolutePath(st.extract.get(MdocPlugin.autoImport.mdocIn))),
+      out = Option(AbsolutePath(st.extract.get(MdocPlugin.autoImport.mdocOut)))
+    )
+    val compileScalacOptions = Project.runTask(scalacOptions.in(Compile), st)
+    val props = compileScalacOptions
+      .map(_._2.toEither)
+      .map(_.map(_.mkString(" ")))
+      .map(_.map(options =>
+        propsDefaultScalacOptions.copy(scalacOptions = options)))
+      .map(_.getOrElse(propsDefaultScalacOptions))
+      .getOrElse(propsDefaultScalacOptions)
+    val settings = Settings
+      .baseDefault(AbsolutePath(PathIO.workingDirectory.toNIO))
+      .withProperties(props)
+    val inputFiles = mdoc.internal.io.IO.inputFiles(settings)
+    if (inputFiles.nonEmpty) {
+      val log = toProcessLogger(st)
+      val sign = st.extract.get(ReleasePlugin.autoImport.releaseVcsSign)
+      val signOff = st.extract.get(ReleasePlugin.autoImport.releaseVcsSignOff)
 
-    val newState = if (status.nonEmpty) {
-      val (state, msg) =
-        st.extract
-          .runTask(ReleaseMdocPlugin.autoImport.releaseMdocCommitMessage, st)
-      vcs(state).commit(msg, sign, signOff) ! log
-      state
+      val vcsInstance = vcs(st)
+      val base = vcsInstance.baseDir.getCanonicalFile
+      val outputFiles = inputFiles.toStream
+        .map(_.out)
+        .map(outputFile => sbt.IO.relativize(base, outputFile.toFile))
+        .filter(_.nonEmpty)
+        .map(_.get)
+      outputFiles.foreach(vcsInstance.add(_) !! log)
+
+      val status = (vcs(st).status !!) trim
+
+      val newState = if (status.nonEmpty) {
+        val (state, msg) =
+          st.extract
+            .runTask(ReleaseMdocPlugin.autoImport.releaseMdocCommitMessage, st)
+        vcs(state).commit(msg, sign, signOff) ! log
+        state
+      } else {
+        // nothing to commit. this happens if the mdoc output files haven't changed.
+        st
+      }
+      newState
     } else {
-      // nothing to commit. this happens if the mdoc input directory hasn't changed.
       st
     }
-    newState
   })
 
   /**
